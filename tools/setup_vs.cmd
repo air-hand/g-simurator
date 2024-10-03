485a1107b229f34b05f0fcce266b47505f7b809c
air-hand <45233435+air-hand@users.noreply.github.com>
2024-10-03 23:26:03 +0900

    fixup

--- tools/setup_vs.cmd
+++ tools/setup_vs.cmd
@@ -1,7 +1,3 @@
-rem @echo off
+@echo off
 
-rem pwsh -NoProfile -Command "Start-Process pwsh.exe -File .\setup_vs.ps1 -Verb RunAs" -WorkingDirectory "%~dp0"
-rem pwsh -NoProfile -Command "Start-Process pwsh.exe -File .\setup_vs.ps1 -Verb RunAs" -WorkingDirectory "%~dp0"
-rem pwsh -Command "Start-Process pwsh.exe -ArgumentList ""-File .\setup_vs.ps1 -Verb RunAs""" -WorkingDirectory "%~dp0"
 pwsh -Command "Start-Process pwsh.exe -ArgumentList ""-File %~dp0\setup_vs.ps1"" -Verb RunAs"
-
