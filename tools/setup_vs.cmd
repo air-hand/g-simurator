86c455958eab978b04b8427a8290fe8a396a92e6
air-hand <45233435+air-hand@users.noreply.github.com>
2024-10-04 00:38:10 +0900

    fix

--- tools/setup_vs.cmd
+++ tools/setup_vs.cmd
@@ -1,4 +1,4 @@
 @echo off
 
-rem pwsh -Command "Start-Process pwsh.exe -ArgumentList ""-File %~dp0\setup_vs.ps1"" -Verb RunAs"
-pwsh %~dp0\setup_vs.ps1
+pwsh -Command "Start-Process pwsh.exe -ArgumentList ""-File %~dp0\setup_vs.ps1"" -Verb RunAs"
+rem pwsh %~dp0\setup_vs.ps1
--- tools/setup_vs.ps1
+++ tools/setup_vs.ps1
@@ -27,8 +27,8 @@ Function InstallBuildTools() {
 #    $vs_args = @("modify", "--quiet", "--productId=${buildtools_product_id}", "--channelId=${channel_id}")
     $vs_args = @("install", "--quiet", "--productId=${buildtools_product_id}", "--channelId=${channel_id}")
     $vs_args += ($vs_components | % { "--add=${_}" })
-    winget uninstall --id Microsoft.VisualStudio.2022.BuildTools --silent
-    winget install --id Microsoft.VisualStudio.2022.BuildTools --override "--quiet --config vsconfig.json" --silent
+    winget uninstall --id Microsoft.VisualStudio.2022.BuildTools --silent --disable-interactivity
+    winget install --id Microsoft.VisualStudio.2022.BuildTools --override "--quiet --config vsconfig.json" --silent --disable-interactivity
 #    Start-Process vs_installer.exe -ArgumentList $vs_args -Wait
 #    Start-Process vs_buildtools.exe -ArgumentList $vs_args -Wait
 #    $vs_components | % {
