58968d74f0ed2b1a6db2811c616fc336c80d607e
air-hand <45233435+air-hand@users.noreply.github.com>
2024-10-04 08:27:23 +0900

    fix

--- tools/build.cmd
+++ tools/build.cmd
@@ -1,4 +1,7 @@
 @echo off
 
+rem call "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars32.bat"
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat"
+
 pwsh %~dp0\build.ps1
 
