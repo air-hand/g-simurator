edf4c89fa1e708b5ef34d4ec2d37c166156cda82
air-hand <45233435+air-hand@users.noreply.github.com>
2024-10-05 14:49:24 +0900

    wip

--- tools/setup_vs.ps1
+++ tools/setup_vs.ps1
@@ -13,27 +13,45 @@ Function InstallBuildTools() {
     $channel_id = "VisualStudio.17.Release"
     $expect_buildtools_name = "Visual Studio Build Tools 2022"
     $buildtools_name = (vswhere.exe -products $buildtools_product_id -property displayName)
-    if ($expect_buildtools_name -cne $buildtools_name) {
-        vs_installer.exe install --productId=$buildtools_product_id --channelId=$channel_id
-#        throw ("BuildTools expect '{0}', but '{1}'" -F $expect_buildtools_name, $buildtools_name)
-    }
-    $vs_components = @(
-        'Microsoft.VisualStudio.Component.VC.Tools.x86.x64'
-        , 'Microsoft.VisualStudio.Component.VC.CMake.Project'
-        , 'Microsoft.VisualStudio.Component.Windows11SDK.26100'
-        , 'Microsoft.VisualStudio.Component.VC.ASAN'
-        , 'Microsoft.VisualStudio.Component.TestTools.BuildTools'
-    )
+    Write-Host $buildtools_name
+#    if ($expect_buildtools_name -cne $buildtools_name) {
+#        vs_installer.exe install --productId=$buildtools_product_id --channelId=$channel_id
+##        throw ("BuildTools expect '{0}', but '{1}'" -F $expect_buildtools_name, $buildtools_name)
+#    }
+#    $vs_components = @(
+#        'Microsoft.VisualStudio.Component.VC.Tools.x86.x64'
+#        , 'Microsoft.VisualStudio.Component.VC.CMake.Project'
+#        , 'Microsoft.VisualStudio.Component.Windows11SDK.26100'
+#        , 'Microsoft.VisualStudio.Component.VC.ASAN'
+#        , 'Microsoft.VisualStudio.Component.TestTools.BuildTools'
+#    )
 #    $vs_args = @("modify", "--quiet", "--productId=${buildtools_product_id}", "--channelId=${channel_id}")
-    $vs_args = @("modify", "--quiet", "--config", "vsconfig.json")
+    $vs_args = @("modify", "--quiet", "--config", ".vsconfig")
     #$vs_args += ($vs_components | % { "--add=${_}" })
-    winget install --id Microsoft.VisualStudio.2022.BuildTools --override "--quiet --config vsconfig.json" --silent --disable-interactivity --accept-source-agreements
+#    winget uninstall --id Microsoft.VisualStudio.2022.BuildTools --silent --disable-interactivity
+    winget install --id Microsoft.VisualStudio.2022.BuildTools --override "--quiet --config .vsconfig" --silent --disable-interactivity --accept-source-agreements
 #    Start-Process vs_installer.exe -ArgumentList $vs_args -Wait
     #Start-Process vs_buildtools.exe -ArgumentList $vs_args -Wait
 #    $vs_components | % {
 #        Start-Process vs_installer.exe -ArgumentList @("modify", "--quiet", "--productId=${buildtools_product_id}", "--channelId=${channel_id}", "--add=${_}") -Wait
 #    }
-    $r = (vswhere.exe -products $buildtools_product_id -property installationPath -requires ("{0}" -F ($vs_components -join " ")))
+
+    $components = (Get-Content -Raw .vsconfig | ConvertFrom-Json).components
+    Write-Host "Requires: ${components}"
+
+    $requires = ($components | % { "-requires {0}" -F $_ })
+    $vswhere_cmd = (
+        "vswhere.exe -products ${buildtools_product_id} -property installationPath ${requires}"
+    )
+    1..50 | % {
+        Write-Host "Waiting for VS Installer... ${_}";
+        Start-Sleep 3;
+        $exists = ($vswhere_cmd | Invoke-Expression)
+        if ($exists) {
+            Write-Host "Installed VS BuildTools at ${exists}"
+            break
+        }
+    }
 }
 
 Function Main() {
