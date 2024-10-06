c4569c91caccdbe5cfde668ca40e065e9017900f
air-hand <45233435+air-hand@users.noreply.github.com>
2024-10-06 19:07:28 +0900

    test

--- tools/build.ps1
+++ tools/build.ps1
@@ -22,4 +22,4 @@ if ($? -ne $true) {
     throw "Build failed."
 }
 # https://discourse.cmake.org/t/ctest-scripting-cmake-presets/9610/2
-ctest --build .\build --preset=test -C Debug
+#ctest --build .\build --preset=test -C Debug
