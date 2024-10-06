c51717cf2174d48b8ffdfe53ab8c78d9e0e2a5c5
air-hand <45233435+air-hand@users.noreply.github.com>
2024-10-06 11:02:10 +0900

    fix

--- tools/build.cmd
+++ tools/build.cmd
@@ -1,9 +1,8 @@
 @echo off
 
-rem call "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars32.bat"
-rem call "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat"
-
 chcp 65001
+
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat"
 
 pwsh %~dp0\build.ps1
 
