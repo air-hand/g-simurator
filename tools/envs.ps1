71936a4ddf584d83dcae48a47b4a9acfdfbb1630
air-hand <45233435+air-hand@users.noreply.github.com>
2024-10-07 22:19:59 +0900

    fix

---
+++ tools/envs.ps1
@@ -0,0 +1,23 @@
+$ErrorActionPreference = 'Stop'
+
+chcp 65001
+
+if ($Env:VCINSTALLDIR -eq $null) {
+    . (Join-Path "${Env:ProgramFiles(x86)}" 'Microsoft Visual Studio\2022\BuildTools\Common7\Tools\Launch-VsDevShell.ps1')
+}
+
+$Env:VCPKG_ROOT = $PSScriptRoot + '\vendor\vcpkg'
+$Env:PATH = "${Env:VCPKG_ROOT};${Env:PATH}"
+if (-not(Test-Path "${Env:VCPKG_ROOT}\.git")) {
+    git clone https://github.com/microsoft/vcpkg.git $Env:VCPKG_ROOT
+    cd $Env:VCPKG_ROOT
+    .\bootstrap-vcpkg.bat
+    cd -
+}
+
+$Env:VCPKG_TARGET_TRIPLET = "x64-windows"
+
+vcpkg install
+
+$vcpkg_tools_path = (vcpkg env --tools "echo %PATH%")
+$Env:PATH = "${vcpkg_tools_path};${Env:PATH}"
