742396a612a56d2b4a6f7cfbbfbdc920b3638e8c
Ryo Shimada <45233435+air-hand@users.noreply.github.com>
2024-10-08 19:12:49 +0900

    envs.ps1 の更新

--- tools/envs.ps1
+++ tools/envs.ps1
@@ -21,7 +21,7 @@ if (-not(Test-Path "${Env:VCPKG_ROOT}\.git")) {
 #$Env:VCPKG_TARGET_TRIPLET = "x64-windows"
 $Env:VCPKG_TARGET_TRIPLET = "x86-windows"
 
-vcpkg install
+vcpkg install --triplet $Env:VCPKG_TARGET_TRIPLET
 
 $vcpkg_tools_path = (vcpkg env --tools "echo %PATH%")
 $Env:PATH = "${vcpkg_tools_path};${Env:PATH}"
