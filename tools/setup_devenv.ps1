52aca8ec879169b34f7b0ccddac5b465a93cdc5d
Ryo Shimada <45233435+air-hand@users.noreply.github.com>
2024-10-22 00:19:41 +0900

    setup_devenv.ps1 の更新

--- tools/setup_devenv.ps1
+++ tools/setup_devenv.ps1
@@ -16,7 +16,7 @@ Function CheckBuildToolsExists()
     $requires = ($components | % { "-requires {0}" -F $_ })
     $vswhere_cmd = (
 #        "vswhere.exe -products ${buildtools_product_id} -property installationPath ${requires}"
-        "vswhere.exe -products * 17.0 -property installationPath ${requires}"
+        "vswhere.exe -products * -version 17.0,17.9 -property installationPath ${requires}"
     )
     $exists = ($vswhere_cmd | Invoke-Expression)
     return $exists
