f5581cbcdf04e0b41d093be7fb36037b057e22a1
Ryo Shimada <45233435+air-hand@users.noreply.github.com>
2024-10-21 23:42:56 +0900

    setup_devenv.ps1 の更新

--- tools/setup_devenv.ps1
+++ tools/setup_devenv.ps1
@@ -27,7 +27,8 @@ Function InstallBuildTools([switch]$clean) {
     if ($clean) {
         winget uninstall --id $winget_build_tools_id --silent --disable-interactivity
     }
-    winget install --id $winget_build_tools_id --override "--quiet --config ${vsconfig}" --silent --disable-interactivity --accept-source-agreements
+    # slow
+#    winget install --id $winget_build_tools_id --override "--quiet --config ${vsconfig}" --silent --disable-interactivity --accept-source-agreements
     foreach ($_ in 1..300) {
         Write-Host "Waiting for VS Installer... ${_}";
         Start-Sleep 3;
