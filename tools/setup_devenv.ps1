5dc9796eceb9c8c35bf4415282b55b9cfb6f23f4
air-hand <45233435+air-hand@users.noreply.github.com>
2024-10-22 20:28:21 +0900

    disable to install cppcheck.

--- tools/setup_devenv.ps1
+++ tools/setup_devenv.ps1
@@ -21,22 +21,22 @@ Function InstallVisualStudio([switch]$clean) {
 }
 
 Function InstallOthers() {
-    winget install -e --id Cppcheck.Cppcheck --silent --disable-interactivity --location "${Env:ProgramFiles}\Cppcheck" --accept-source-agreements
-    if (-not(Get-Command cppcheck.exe -ErrorAction SilentlyContinue)) {
-        Write-Host "Setup path to Cppcheck..."
-        $cppcheck_path = "${Env:ProgramFiles}\Cppcheck"
-        $user_path = [System.Environment]::GetEnvironmentVariable("PATH", "User")
-        $cppcheck_path_exists = @($user_path -split ';') -contains $cppcheck_path
-        if ($Env:GITHUB_ACTIONS) {
-            Write-Host "Run on GitHub Actions"
-            $Env:PATH += ";${cppcheck_path}"
-            Set-Content -Path $Env:GITHUB_PATH $Env:PATH
-        } elseif (-not($cppcheck_path_exists)) {
-            $user_path += ";${cppcheck_path}"
-            [System.Environment]::SetEnvironmentVariable("PATH", $user_path, "User")
-            Write-Host "Restart vscode or powershell process to reflect the changes."
-        }
-    }
+#    winget install -e --id Cppcheck.Cppcheck --silent --disable-interactivity --location "${Env:ProgramFiles}\Cppcheck" --accept-source-agreements
+#    if (-not(Get-Command cppcheck.exe -ErrorAction SilentlyContinue)) {
+#        Write-Host "Setup path to Cppcheck..."
+#        $cppcheck_path = "${Env:ProgramFiles}\Cppcheck"
+#        $user_path = [System.Environment]::GetEnvironmentVariable("PATH", "User")
+#        $cppcheck_path_exists = @($user_path -split ';') -contains $cppcheck_path
+#        if ($Env:GITHUB_ACTIONS) {
+#            Write-Host "Run on GitHub Actions"
+#            $Env:PATH += ";${cppcheck_path}"
+#            Set-Content -Path $Env:GITHUB_PATH $Env:PATH
+#        } elseif (-not($cppcheck_path_exists)) {
+#            $user_path += ";${cppcheck_path}"
+#            [System.Environment]::SetEnvironmentVariable("PATH", $user_path, "User")
+#            Write-Host "Restart vscode or powershell process to reflect the changes."
+#        }
+#    }
 }
 
 Function Main() {
