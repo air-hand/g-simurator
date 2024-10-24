1edea6a9b06085d2c6866520ed0ba72391ce16af
Ryo Shimada <45233435+air-hand@users.noreply.github.com>
2024-10-24 23:07:09 +0900

    x64-windows.cmake の更新

--- triplets/x64-windows.cmake
+++ triplets/x64-windows.cmake
@@ -3,5 +3,5 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
 # /permissive for protobuf.
-set(VCPKG_CXX_FLAGS "/std:c++20 /Zc:__cplusplus /permissive /utf-8" CACHE STRING "")
+set(VCPKG_CXX_FLAGS "/std:c++latest /Zc:__cplusplus /permissive /utf-8" CACHE STRING "")
 set(VCPKG_C_FLAGS "/std:c17 /utf-8" CACHE STRING "")
