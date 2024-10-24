eb57f0e35fc7fc57bed30f9493db8b733e781cc0
Ryo Shimada <45233435+air-hand@users.noreply.github.com>
2024-10-24 23:07:23 +0900

    x64-windows-static.cmake の更新

--- triplets/x64-windows-static.cmake
+++ triplets/x64-windows-static.cmake
@@ -2,5 +2,5 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 
-set(VCPKG_CXX_FLAGS "/std:c++20 /Zc:__cplusplus /permissive /utf-8" CACHE STRING "")
+set(VCPKG_CXX_FLAGS "/std:c++latest /Zc:__cplusplus /permissive /utf-8" CACHE STRING "")
 set(VCPKG_C_FLAGS "/std:c17 /utf-8" CACHE STRING "")
