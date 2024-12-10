65e6b0ffb3c4aaf9c3836f16ed957fcb327c2638
air-hand <45233435+air-hand@users.noreply.github.com>
2024-12-11 08:56:46 +0900

    fix

--- src/tests/test_image.cpp
+++ src/tests/test_image.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <cuda_runtime.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/cuda.hpp>
 #include <opencv2/cudaarithm.hpp>
@@ -13,8 +14,19 @@ namespace
 
 namespace image = sim::utils::image;
 
+bool IsCudaAvailable()
+{
+    int device_count = 0;
+    cudaError_t error_id = cudaGetDeviceCount(&device_count);
+    return error_id == cudaSuccess && device_count > 0;
+}
+
 TEST(test_image, crop)
 {
+    if (!IsCudaAvailable())
+    {
+        GTEST_SKIP() << "CUDA is not available.";
+    }
     cv::cuda::GpuMat input(100, 100, CV_8UC4);
     cv::cuda::GpuMat foo = input(cv::Rect(10, 10, 20, 20));
     cv::cuda::GpuMat output;
