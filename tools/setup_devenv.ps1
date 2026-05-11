58cc17dbd6e07d0970818e17adde463085dbf6a1
air-hand <45233435+air-hand@users.noreply.github.com>
2026-05-11 19:03:58 +0900

    feat!: use vs2026

--- tools/setup_devenv.ps1
+++ tools/setup_devenv.ps1
@@ -5,28 +5,36 @@
 Import-Module $PSScriptRoot\choco_utils.psm1
 Import-Module $PSScriptRoot\vs_utils.psm1
 
-Function InstallVisualStudio([boolean]$clean) {
-    if (-not($clean) -and (PathToVisualStudio -with_vsconfig)) {
+Function InstallVisualStudio([boolean]$clean)
+{
+    if (-not($clean) -and (PathToVisualStudio -with_vsconfig))
+    {
         Write-Host "VisualStudio is already installed."
         return
     }
     $installed_path = (PathToVisualStudio)
-    if ($installed_path -and -not($clean)) {
+    if ($installed_path -and -not($clean))
+    {
         ModifyVSWithConfig $installed_path
-    } else {
+    } else
+    {
         # The Community edition is compatible with The Enterprise edition. https://github.com/actions/runner-images/blob/main/images/windows/Windows2022-Readme.md#visual-studio-enterprise-2022
-        $winget_visualstudio_id = "Microsoft.VisualStudio.2022.Community"
+        # VS 2026 -> VisualStudio.Community
+        $winget_visualstudio_id = "Microsoft.VisualStudio.Community"
         $vsconfig = ".vsconfig"
-        if ($clean) {
+        if ($clean)
+        {
             winget uninstall --id $winget_visualstudio_id -e --silent --disable-interactivity
         }
         winget install --id $winget_visualstudio_id -e --override "--quiet --config ${vsconfig}" --silent --disable-interactivity --accept-source-agreements
     }
-    foreach ($_ in 1..300) {
+    foreach ($_ in 1..300)
+    {
         Write-Host "Waiting for VS Installer... ${_}";
         Start-Sleep 3;
         $exists = PathToVisualStudio -with_vsconfig
-        if ($exists) {
+        if ($exists)
+        {
             Write-Host "Installed VisualStudio at ${exists}"
             return
         }
@@ -34,9 +42,11 @@ Function InstallVisualStudio([boolean]$clean) {
     throw "VisualStudio not found."
 }
 
-Function InstallOthers([boolean]$clean) {
+Function InstallOthers([boolean]$clean)
+{
     $cuda_version = "12.6"
-    if ($clean) {
+    if ($clean)
+    {
         winget uninstall -e --id Nvidia.CUDA -v $cuda_version --silent --disable-interactivity
     }
     # https://docs.nvidia.com/cuda/cuda-installation-guide-microsoft-windows/#id3
@@ -64,18 +74,21 @@ Function InstallOthers([boolean]$clean) {
     winget install -e --id Nvidia.CUDA -v $cuda_version --source winget --silent --disable-interactivity --accept-source-agreements `
         --override ("-s {0} -n" -F ($sub_packages -join " "))
     $installed_path = "${Env:ProgramFiles}\NVIDIA GPU Computing Toolkit\CUDA\v${cuda_version}"
-    if (-not(Test-Path $installed_path)) {
+    if (-not(Test-Path $installed_path))
+    {
         throw "CUDA not found."
     }
     $Env:CUDA_PATH = $installed_path
     [System.Environment]::SetEnvironmentVariable("CUDA_PATH", $Env:CUDA_PATH, [System.EnvironmentVariableTarget]::User)
-    if (($Env:PATH -split ";") -notcontains "${Env:CUDA_PATH}\bin") {
+    if (($Env:PATH -split ";") -notcontains "${Env:CUDA_PATH}\bin")
+    {
         $Env:PATH = "${Env:CUDA_PATH}\bin;${Env:PATH}"
         [System.Environment]::SetEnvironmentVariable("PATH", $Env:PATH, [System.EnvironmentVariableTarget]::User)
     }
 }
 
-Function Main([boolean]$clean) {
+Function Main([boolean]$clean)
+{
     pushd $PSScriptRoot > $null
 
     SetupPathToVSInstaller
--- tools/vs_utils.psm1
+++ tools/vs_utils.psm1
@@ -4,43 +4,49 @@ Function Invoke-ProcessStreamed(
     [string[]]$ArgumentList=@()
 )
 {
-    try {
-      $psi = [ProcessStartInfo]::new()
-      $psi.FileName = $FilePath
-      $psi.Arguments = ($ArgumentList -join ' ')
-      $psi.RedirectStandardOutput = $true
-      $psi.RedirectStandardError  = $true
-      $psi.UseShellExecute = $false
-      $psi.CreateNoWindow  = $true
-      $p = [Process]::new();
-      $p.StartInfo = $psi
-      $p.EnableRaisingEvents = $true
-      $script = {
-        $data = $Event.SourceEventArgs.Data
-        if (-not [string]::IsNullOrEmpty($data)) {
-            [System.Console]::WriteLine($data)
+    try
+    {
+        $psi = [ProcessStartInfo]::new()
+        $psi.FileName = $FilePath
+        $psi.Arguments = ($ArgumentList -join ' ')
+        $psi.RedirectStandardOutput = $true
+        $psi.RedirectStandardError  = $true
+        $psi.UseShellExecute = $false
+        $psi.CreateNoWindow  = $true
+        $p = [Process]::new();
+        $p.StartInfo = $psi
+        $p.EnableRaisingEvents = $true
+        $script = {
+            $data = $Event.SourceEventArgs.Data
+            if (-not [string]::IsNullOrEmpty($data))
+            {
+                [System.Console]::WriteLine($data)
+            }
         }
-      }
-      $stdoutEvent = Register-ObjectEvent -InputObject $p -EventName OutputDataReceived -Action $script
-      $stderrEvent = Register-ObjectEvent -InputObject $p -EventName ErrorDataReceived -Action $script
-      [void]$p.Start()
-      $p.BeginOutputReadLine(); $p.BeginErrorReadLine();
-      $p.WaitForExit()
-      $p.CancelOutputRead(); $p.CancelErrorRead()
-      Unregister-Event -SourceIdentifier $stdoutEvent
-      Unregister-Event -SourceIdentifier $stderrEvent
-      return $p.ExitCode
-    } finally {
-        if ($p) {
+        $stdoutEvent = Register-ObjectEvent -InputObject $p -EventName OutputDataReceived -Action $script
+        $stderrEvent = Register-ObjectEvent -InputObject $p -EventName ErrorDataReceived -Action $script
+        [void]$p.Start()
+        $p.BeginOutputReadLine(); $p.BeginErrorReadLine();
+        $p.WaitForExit()
+        $p.CancelOutputRead(); $p.CancelErrorRead()
+        Unregister-Event -SourceIdentifier $stdoutEvent
+        Unregister-Event -SourceIdentifier $stderrEvent
+        return $p.ExitCode
+    } finally
+    {
+        if ($p)
+        {
             $p.Dispose()
         }
     }
 }
 
-Function SetupPathToVSInstaller() {
+Function SetupPathToVSInstaller()
+{
     $vswhere = Join-Path "${Env:ProgramFiles(x86)}" "Microsoft Visual Studio\Installer\vswhere.exe"
 
-    If (-not(Test-Path $vswhere)) {
+    If (-not(Test-Path $vswhere))
+    {
         throw "vswhere.exe not found."
     }
 
@@ -50,9 +56,10 @@ Function SetupPathToVSInstaller() {
 Function PathToVisualStudio([switch]$with_vsconfig)
 {
     $vswhere_cmd = (
-        "vswhere.exe -products * -version 17.0,18.0 -property installationPath"
+        "vswhere.exe -products * -version 18.0,19.0 -property installationPath"
     )
-    if ($with_vsconfig) {
+    if ($with_vsconfig)
+    {
         $vsconfig = Join-Path $PSScriptRoot ".vsconfig"
         $components = (Get-Content -Raw $vsconfig | ConvertFrom-Json).components
         $requires = ($components | % { "-requires {0}" -F $_ })
@@ -66,11 +73,11 @@ Function ModifyVSWithConfig([string]$installed_path)
     $vsconfig = Join-Path $PSScriptRoot ".vsconfig"
     $vs_installer = Get-Command "vs_installer.exe"
     $vs_installer_args = (@(
-        "modify",
-        "--installPath", "`"${installed_path}`"",
-        "--config", "`"${vsconfig}`"",
-        "--quiet", "--norestart", "--force", "--downloadThenInstall"
-    ))
+            "modify",
+            "--installPath", "`"${installed_path}`"",
+            "--config", "`"${vsconfig}`"",
+            "--quiet", "--norestart", "--force", "--downloadThenInstall"
+        ))
 
     $cmd = ("{0} {1}" -F $vs_installer.Name, ($vs_installer_args -join " "))
 
