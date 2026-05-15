c5586e0e7c009aa2669a114c720f2ea46e68f178
air-hand <45233435+air-hand@users.noreply.github.com>
2026-05-15 16:28:53 +0900

    custom-toolchain.cmake の更新

--- custom-toolchain.cmake
+++ custom-toolchain.cmake
@@ -1,3 +1,10 @@
+if(DEFINED _VCPKG_ROOT_DIR)
+  message(STATUS "_VCPKG_ROOT_DIR ${_VCPKG_ROOT_DIR}")
+endif()
+if(DEFINED ENV{VCPKG_ROOT})
+  message(STATUS "ENV{VCPKG_ROOT} $ENV{VCPKG_ROOT}")
+endif()
+
 include("$ENV{VCPKG_ROOT}/scripts/toolchains/windows.cmake")
 
 set(CMAKE_CUDA_ARCHITECTURES "86" CACHE STRING "")
