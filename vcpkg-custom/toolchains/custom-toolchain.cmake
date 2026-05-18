496ab1c50a0ae8e2b9c117d17666a8062a679e14
air-hand <45233435+air-hand@users.noreply.github.com>
2026-05-18 13:46:39 +0900

    fix: pass CMake settings to vcpkg ports (#19)
    * clean: delete unused static triplet
    
    * fix: use custom toolchain to pass cmake vars to vcpkg ports
    
    * chore: update builtin-baseline
    
    * fix: pull CUDA 13.2 fixed version
    
    * fix: pull CUDA 13.2 fixed version
    
    * fix: pin CUDA 13.2 fixed version
    
    * chore: add comment

---
+++ vcpkg-custom/toolchains/custom-toolchain.cmake
@@ -0,0 +1,13 @@
+# need VCPKG_KEEP_ENV_VARS and VCPKG_ENV_PASSTHROUGH_UNTRACKED to use ENV{VCPKG_ROOT} on Windows
+include("$ENV{VCPKG_ROOT}/scripts/toolchains/windows.cmake")
+
+set(CMAKE_CXX_STANDARD 23 CACHE STRING "")
+set(CMAKE_CXX_STANDARD_REQUIRED ON CACHE BOOL "")
+
+set(CMAKE_CUDA_STANDARD 20 CACHE STRING "")
+# forward to all ports by vcpkg
+# https://developer.nvidia.com/cuda-gpus
+# GeForce RTX 30 Series (Ampere): 8.6
+# GeForce RTX 40 Series (Ada Lovelace): 8.9
+set(CMAKE_CUDA_ARCHITECTURES "86" CACHE STRING "")
+#set(CMAKE_CUDA_ARCHITECTURES "86;89" CACHE STRING "")
